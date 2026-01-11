ecba1b8cb335cf112b75db2e1676ca0c2f7f820a
Eranga <erangakavishanka@icloud.com>
2026-01-11 22:03:37 +0530

    got a vegie list

--- app/products/veggies/page.tsx
+++ app/products/veggies/page.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 
 function page() {
+  const vegies = [
+
+    {
+      name: "broccali",
+      id: 1
+    },
+    {
+      name: "spinach",
+      id: 2
+    },
+    {
+      name: "carrot",
+      id: 3 
+    },
+    {
+      name: "potato",
+      id: 4
+    }
+  ]
   return (
     <div>
       <h1>veggies page</h1>
-      <ul>
-        <li><Link href="/products/veggies/broccali">Broccali</Link></li>
-        <li><Link href="/products/veggies/spinach">Spinach</Link></li>
-      </ul>
+     
       
     </div>
   )
