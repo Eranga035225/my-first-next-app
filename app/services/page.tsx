6837963ae651650f5fe0a818548aec18c7536b89
Eranga <erangakavishanka@icloud.com>
2026-01-11 21:53:07 +0530

    added  a back button

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 function page() {
   return (
-  <h1>Services page</h1>
+    <>
+      <h1>Services page</h1>
+  <Link href="/">Home</Link>
+    
+    </>
   )
 }
 
