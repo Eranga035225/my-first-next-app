13c7e9a8c2a1c230193f562b667d926bde7a48c5
Eranga <erangakavishanka@icloud.com>
2026-01-11 21:32:41 +0530

    old method

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -7,8 +7,8 @@ export default function Home() {
 
    <h1>Home Page</h1>
    <ul>
-    <li>Home</li>
-    <li>About</li>
+    <li><a href="/my-first-next-app">Home</a></li>
+    <li><a href="/services">Services</a></li>
     <li>Services</li>
     <li>Contact</li>
     <li>Products</li>
