3b54162836fa64a32c2a0d02f70df34d2e3f1582
Eranga <erangakavishanka@icloud.com>
2026-01-11 21:02:36 +0530

    implemenmted mapping catch all routes

--- app/[...dpath]/page.tsx
+++ app/[...dpath]/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
-function page() {
+ async function page(
+  {params,}
+  : {
+    params : Promise<{ dpath: string[] }>,
+  }
+
+
+) {
+
+  const { dpath } = await params
+
+
+
+
   return (
+    <>
     <div>
       <h1>Catch All Routes</h1>
       
     </div>
+    <ul>
+      {dpath.map((item)=>{
+        return (
+          <li key={item}>{item}</li>
+
+        )
+      })}
+
+
+
+
+    </ul>
+    
+    
+    </>
+    
   )
 }
 
