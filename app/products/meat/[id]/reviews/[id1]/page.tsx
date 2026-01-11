b86bad8896de9763d90750bf7949503ec2155140
Eranga <erangakavishanka@icloud.com>
2026-01-11 20:36:08 +0530

    dynakically nested routing

--- app/products/meat/[id]/reviews/[id1]/page.tsx
+++ app/products/meat/[id]/reviews/[id1]/page.tsx
@@ -1,13 +1,13 @@
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id1: string }>
+  params: Promise<{ id1: string, id : string }>
 }) {
-  const { id1 } = await params
+  const { id1, id } = await params
 
   return (
     <div>
-      Meat item  {id1} reviews
+      Meat item  {id} reviews {id1}
     </div>
   )
 }
